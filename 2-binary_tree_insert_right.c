24489a25e5c8436c054d34e21e6645f2e0ded082
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-12-13 15:14:24 +0100

    feat(insert-right): function that inserts to the right of parent node

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - inserts a node as the left-child of another node
+ * @parent: a pointer to the parent node of the node to create
+ * @value: the value to put in the new node
+ *
+ * Return: a pointer to the new node or NULL
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	/*create pointer and allocate memory for the new node*/
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *old_child;
+
+	/*check if mem alloc fails or parent is NULL*/
+	if (!new_node || !parent)
+	{
+		return (NULL);
+	}
+
+	/*point new_node->parent to the given parents*/
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	/*if the parent initially had a right-child*/
+	if (parent->right != NULL)
+	{
+		old_child = parent->right;
+		/*point old_child->parent to new_node*/
+		old_child->parent = new_node;
+		/*point new_node right to old right child*/
+		new_node->right = old_child;
+	}
+	/*now point it to the right of the parent*/
+	parent->right = new_node;
+
+	return (new_node);
+}
