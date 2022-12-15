4404be4209c965c42ecc0fc25393d7a3a896f046
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-12-15 07:49:48 +0100

    feat(rotate_tree_left): Rotates a tree left

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_rotate_left - performs a left-rotation on a binary tree
+ * @tree: pointer to the root node
+ * Return: pointer to the new root node
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *new_root;
+
+	if (!tree)
+		return (NULL);
+
+	new_root = tree->right;
+
+	/* rotating of parents */
+	new_root->parent = tree->parent;
+	tree->parent = new_root;
+	if (new_root->left)
+		(new_root->left)->parent = tree;
+
+	/* rotating of children */
+	tree->right = new_root->left;
+	new_root->left = tree;
+
+	return (new_root);
+}
