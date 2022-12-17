ee642efaa7433c14e3eb02405400033a915d2800
Certifieddonnie <mcdonaldsamure91@gmail.com>
2022-12-17 02:35:59 +0100

    Big O #Binary Heap

---
+++ mains/102-main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/* Our own functions */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
+void binary_tree_delete(binary_tree_t *tree);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	binary_tree_t *root;
+	int complete;
+
+	root = binary_tree_node(NULL, 98);
+
+	complete = binary_tree_is_complete(root);
+	printf("Is the tree with root node (%d) complete? %d\n", root->n, complete);
+
+	binary_tree_print(root);
+
+	binary_tree_delete(root);
+	return (0);
+}
